3d63440647610fec1ad30520004772fc30977727
Hyghdrogin <omokewt@gmail.com>
2024-03-22 11:28:23 +0100

    Fix: Unshorten Link

--- src/routes/url.ts
+++ src/routes/url.ts
@@ -1,13 +1,14 @@
 import { Router as URLRouter } from "express";
 import { verifyUserToken } from "../middleware";
 import {
-	createURL, customURL, viewLink, viewLinks, getURLAnalytics
+	createURL, customURL, viewLink, viewLinks, getURLAnalytics, unshortenURL
 } from "../controllers/url";
 
 const router = URLRouter();
 
 router.post("/", verifyUserToken, createURL);
 router.post("/custom", verifyUserToken, customURL);
+router.post("/unshorten", verifyUserToken, unshortenURL);
 router.get("/links", verifyUserToken, viewLinks);
 router.get("/:shortCode", viewLink);
 router.get("/analytics/:shortCode", verifyUserToken, getURLAnalytics);
